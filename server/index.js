9ff4a568d1bd3274ab49d1a65700f25db131c7e3
Aaron Miller <AaronMiller1855@gmail.com>
2023-12-21 08:01:59 -0700

    create connection to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../db/database')
 
 
 const app = express();
@@ -9,6 +10,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+// Check database connection
+db.connect(error => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    return;
+  }
+  console.log('Successfully connected to the database.');
+});
 
 
 app.listen(PORT, () => {
